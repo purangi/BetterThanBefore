bfd227c0e59297bf6e2c939713bb9644ef09d420
purangi <kaya0728@naver.com>
2022-07-11 17:48:30 +0900

    메인 맵 마우스 클릭 이벤트 구현
    곡예사만 적용 commit

---
+++ BetterThanBefore/Assets/Script/CSHouseClick.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CSImageLine : MonoBehaviour
+{
+    private Image FirstImage; //기존 이미지
+    public Sprite nextImage; //변경 테두리 이미지
+    public Sprite basicImage; //기본 테두리 이미지
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirstImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeImage()
+    {
+        FirstImage.sprite = nextImage; //마우스 오버시 테두리 이미지 출력
+    }
+
+    public void BasicImage()
+    {
+        FirstImage.sprite = basicImage; //마우스 나갔을때
+    }
+}
