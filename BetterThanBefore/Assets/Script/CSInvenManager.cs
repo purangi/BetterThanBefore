d8b7fa71385d6c10c3ab3f529675ffad2c1a23a0
purangi <kaya0728@naver.com>
2022-07-21 01:16:55 +0900

    Knight Scene 합치기
    편지, 액자, 인벤토리, 골드 구현 추가 commit

--- BetterThanBefore/Assets/Script/CSInvenManager.cs
+++ BetterThanBefore/Assets/Script/CSInvenManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class CSInvenManager : MonoBehaviour
 {
+    //public List<CSInvenManager> items;
     private GameObject InvenObject;
+    // public GameObject[] slots;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,16 @@ public class CSInvenManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    /*아이템 획득 시.. (위 주석처리도 이에 필요한 것)
+    public void GotItem(획득한 아이템) {
+    if ( items.Count < slots.Length ) {
+    items.Add(아이템);
+    } else {
+    print("인벤토리가 꽉 찼습니다.");
+    }
+    }*/
+
 }
--- BetterThanBefore/Assets/Script/KnightHButton.cs
+++ BetterThanBefore/Assets/Script/KnightHButton.cs
@@ -13,16 +13,8 @@ public class KnightHButton : MonoBehaviour
     private GameObject candleObject;
     private GameObject keyObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    bool LetterChk = false;
+    bool PhotoChk = false;
 
     public void GetCandle() //커서 변경
     {
@@ -56,6 +48,62 @@ public class KnightHButton : MonoBehaviour
         } else
         {
             Debug.Log("실패");
+        }
+    }
+
+    public void LetterClickExpand() //편지 확대
+    {
+        RectTransform rectTran = gameObject.GetComponent<RectTransform>();
+        GameObject obj = GameObject.Find("KHLetter");
+        Vector3 position = obj.transform.localPosition;
+
+        if (LetterChk == false)
+        {
+            rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 300);
+            rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 450);
+            position.x = 0;
+            position.y = 0;
+            obj.transform.localPosition = position;
+            obj.transform.localPosition = position;
+            LetterChk = true;
+        }
+
+        else
+        {
+            rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 130);
+            rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 230);
+            position.x = -172;
+            position.y = -52;
+            obj.transform.localPosition = position;
+            LetterChk = false;
+        }
+    }
+
+    public void PhotoClickExpand() //사진 확대
+    {
+        RectTransform rectTran = gameObject.GetComponent<RectTransform>();
+        GameObject obj = GameObject.Find("KHPhoto");
+        Vector3 position = obj.transform.localPosition;
+
+        if (PhotoChk == false)
+        {
+            rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 800);
+            rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 760);
+            obj.transform.SetAsLastSibling();
+            position.x = 0;
+            position.y = 0;
+            obj.transform.localPosition = position;
+            PhotoChk = true;
+        }
+
+        else
+        {
+            rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 200);
+            rectTran.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 190);
+            position.x = -336;
+            position.y = 214;
+            obj.transform.localPosition = position;
+            PhotoChk = false;
         }
     }
 }
