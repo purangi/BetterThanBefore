dd36193e82bfd61d4fcb79d0e1380a9cb51539fd
purangi <kaya0728@naver.com>
2023-12-28 10:22:13 +0900

    organize directory

--- BetterThanBefore/Library/PackageCache/dev.yarnspinner.unity@2.2.1/Editor/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyVersion("2.2.1.0")]
-[assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
-
-[assembly: AssemblyFileVersion("2.2.1.0")]
-[assembly: AssemblyInformationalVersion("2.2.1.0")]
